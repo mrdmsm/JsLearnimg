d0d7c2820999dc889eee278119cc73414ec13737
mrdmsm <92385017+mrdmsm@users.noreply.github.com>
2025-06-09 13:22:47 +0330

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+// // let username;
+// // let password;
+// // document.getElementById("submit").onclick = function() {
+// //     username = document.getElementById("mytext").value;
+// //     password = document.getElementById("mypass").value;
+// //     window.alert(`your username is ${username} and youu password is ${password}`)
+// // }
+
+
+// let username ;
+// let password ;
+
+// username = window.prompt("enter your name");
+// password = window.prompt("enter your password");
+
+// window.alert(`your name is ${username} and your password is ${password}`)
+
+let num1 = 20;
+let num2 = 14;
+
+num1 += num2
+num1 = num1 + num2
+
+console.log(num1);
---
+++ index2.js
@@ -0,0 +1,6 @@
+// let age = window.prompt("enter your age ")
+// age = Number()
+
+// age +=1;
+
+// age=age+1;
