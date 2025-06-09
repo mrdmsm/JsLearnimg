d52b2c768255e4cd98a48d692a02b7763808d62e
mrdmsm <92385017+mrdmsm@users.noreply.github.com>
2025-06-09 13:45:27 +0330

    index2.js

--- index2.js
+++ index2.js
@@ -4,3 +4,12 @@
 // age +=1;
 
 // age=age+1;
+let username;
+let password;
+document.getElementById("submit").onclick = function() {
+    username = document.getElementById("mytext").value;
+    password = document.getElementById("mypass").value;
+    window.alert(`your username is ${username} and youu password is ${password}`)
+}
+
+
