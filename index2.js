b64b00c8695d5e2845f7b6185f7c09a1746fa560
mrdmsm <92385017+mrdmsm@users.noreply.github.com>
2025-06-09 13:48:28 +0330

    index2.js

--- index2.js
+++ index2.js
@@ -9,7 +9,7 @@ let password;
 document.getElementById("submit").onclick = function() {
     username = document.getElementById("mytext").value;
     password = document.getElementById("mypass").value;
-    window.alert(`your username is ${username} and youu password is ${password}`)
+    // window.alert(`your username is ${username} and youu password is ${password}`)
     console.log(`The user username is ${username} and his password is ${password}`)
 }
 
