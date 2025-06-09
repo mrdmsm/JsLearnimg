8feb7bc4dde0a9ed0d128e595432459beab2d907
mrdmsm <92385017+mrdmsm@users.noreply.github.com>
2025-06-09 13:51:39 +0330

    index2.js

--- index2.js
+++ index2.js
@@ -1,16 +1,8 @@
-// let age = window.prompt("enter your age ")
-// age = Number()
-
-// age +=1;
-
-// age=age+1;
 let username;
 let password;
 document.getElementById("submit").onclick = function() {
     username = document.getElementById("mytext").value;
     password = document.getElementById("mypass").value;
-    // window.alert(`your username is ${username} and youu password is ${password}`)
-    console.log(`The user username is ${username} and his password is ${password}`)
+    window.alert(`your username is ${username} and youu password is ${password}`)
+    console.log(`his username is ${username} and his password is ${password}`)
 }
-
-
